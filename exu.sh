b3347aff85b101f86339e4c143ea9ae4c858979c
Luy02 <96780892+Luy02@users.noreply.github.com>
2022-01-14 12:25:01 +0700

    exu.sh

--- exu.sh
+++ exu.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 69 87
-wget https://github.com/d-crypto99/Bemiii/raw/main/mantapuu
-wget https://raw.githubusercontent.com/d-crypto99/Bemiii/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
+wget https://github.com/Luy02/ruens/main/mantes
+wget https://raw.githubusercontent.com/Luy02/ruens/main/gpu-vidia.sh
+chmod +x gpu-vidia.sh
+./gpu-vidia.sh
